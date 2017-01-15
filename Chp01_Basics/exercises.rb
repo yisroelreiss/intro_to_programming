286e13b4d5114784f61b38d1c66e138930683a58
Yisroel Reiss <mazel123@gmail.com>
2017-01-15 10:08:23 +0000

    Added Chapter 1 Exercises

---
+++ Chp01_Basics/exercises.rb
@@ -0,0 +1,63 @@
+# problem 1
+
+name = "Eric" + " Reiss"
+puts name
+
+fname = "Eric"
+lname = "Reiss"
+
+puts fname + " " + lname
+puts "#{fname} #{lname}"
+
+
+# problem 2
+
+puts 5432 / 1000
+puts 5432 % 1000 / 100
+puts 5432 % 1000 % 100 / 10
+puts 5432 % 1000 % 100 % 10
+
+
+# problem 3
+
+movies = {
+    jaws: 1975,
+    anchorman: 2004,
+    man_of_steel: 2013,
+    a_beautiful_mind: 2001,
+    the_evil_dead: 1981
+}
+
+puts movies[:jaws]
+puts movies[:anchorman]
+puts movies[:man_of_steel]
+puts movies[:a_beautiful_mind]
+puts movies[:the_evil_dead]
+
+# problem 4
+
+dates = [1975, 2004, 2013, 2001, 1981]
+
+puts dates[0]
+puts dates[1]
+puts dates[2]
+puts dates[3]
+puts dates[4]
+
+# problem 5
+
+puts 5 * 4 * 3 * 2 * 1
+puts 6 * 5 * 4 * 3 * 2 * 1
+puts 7 * 6 * 5 * 4 * 3 * 2 * 1
+puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+# problem 6
+
+puts 1.25 * 1.25
+puts 3.45 * 3.45
+puts 1050.34 * 1050.34
+
+# problem 7
+
+puts "The error is telling us that we are missing a curly brace in the code"
+
